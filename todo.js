b1ef0b92fed284994e2400241095fd653b6b051e
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-01-29 20:28:08 -0800

    finished basic todo

--- todo.js
+++ todo.js
@@ -2,15 +2,31 @@ const list = document.getElementById("todo-list");
 const field = document.getElementById("task-field");
 
 function addToList() {
+  if (field.value === "") {
+    alert("Please enter in a task!");
+    return;
+  }
   const task = document.createElement("li");
-  task.innerHTML = field.value;
+  task.textContent = field.value;
   list.appendChild(task);
+
+  const x_button = document.createElement("span");
+  x_button.className = "close";
+  x_button.textContent = "x";
+
+  task.appendChild(x_button);
+
+  x_button.onclick = function () {
+    removeTask(this);
+  };
+
   task.onclick = function () {
     remove(this);
   };
-  field.innerHTML = "";
+  field.value = "";
 }
 
-function remove(e) {
-  e.parentNode.removeChild(e);
+function removeTask(e) {
+  li_task = e.parentNode;
+  li_task.parentNode.removeChild(li_task);
 }
